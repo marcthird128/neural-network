6c9865547e114f88f00a3c6e349d41ccd118d3f5
Marc Platt <plattmmarc12@gmail.com>
2025-04-08 10:45:40 -0700

    Added config

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
-print("Hello world")
+# imports
+import json
+
+# open configuration file
+config_file = open('config.json', 'r')
+config = config_file.read()
+config = json.loads(config);
+
+# print initial message
+print(config['name']);
